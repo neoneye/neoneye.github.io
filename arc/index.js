2318bb1b676ffdd38ca609bf1ad116c60d7c66e8
Simon Strandgaard <neoneye@gmail.com>
2024-01-22 17:49:54 +0100

    Snapshot of my ARC-Interactive project as it is.

--- arc/index.js
+++ arc/index.js
@@ -44,38 +44,9 @@ class PageController {
     async loadNames() {
         console.log('PageController.loadNames()');
         let names = [
-            '0a2355a6',
-            '0b17323b',
-            '0bb8deee',
-            '0becf7df',
-            '0c786b71',
-            '0c9aba6e',
-            'landport_rotrev_00_56_33_87_02_54_99_68',
-            'landport_rotrev_02_44_77_37_54_00',
-            'ExtractObjects1',
-            'ExtractObjects2',
-            'ExtractObjects3',
-            'InsideOutside1',
-            'InsideOutside2',
-            'InsideOutside3',
-            'after_rain_l6bv83jfqxba4q8hzbg',
-            'align_based_on_the_arrow_direction_l6afyzc2pjsb51gkdg',
-            'AscendingOrder_l6abjqzwwo7mhobq52',
             '0a0a50ad',
             '0a0ac772',
             '0a0adaff',
-            '3128a01f',
-            '3875e525',
-            '4325b823',
-            'closure-filling-0',
-            'closure-filling-1',
-            'closure-filling-2',
-            'shape-pattern-similarity-0',
-            'shape-pattern-similarity-1',
-            'shape-pattern-similarity-2',
-            'referenceable-components',
-            'surface-pixel-count',
-            'vyaguzeoi',
         ];
 
         let new_names = [];
