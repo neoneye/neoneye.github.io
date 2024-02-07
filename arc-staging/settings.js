0676a9a90e14e055bbc55393e587994ab6ab12bc
Simon Strandgaard <neoneye@gmail.com>
2024-02-07 22:42:54 +0100

    Snapshot of my ARC-Interactive project as it is.

--- arc-staging/settings.js
+++ arc-staging/settings.js
@@ -17,6 +17,46 @@ class PageController {
 
         this.updateColorPreview(false);
         this.updateCallbackUrl();
+
+    
+        this.setupSimpleAdvancedToggle();
+    }
+
+    // Simple / Advanced mode handling
+
+    setupSimpleAdvancedToggle() {
+        if (Settings.getAdvancedModeEnabled()) {
+            this.showAdvancedSettings();
+        } else {
+            this.hideAdvancedSettings();
+        }
+
+        document.getElementById('simple-advanced-toggle-button').addEventListener('click', () => {
+            var el_advanced = document.getElementById('advanced-settings');
+            var advanceModeEnabled = false;
+            if (el_advanced.classList.contains('hidden')) {
+                this.showAdvancedSettings();
+                advanceModeEnabled = true;
+            } else {
+                this.hideAdvancedSettings();
+                advanceModeEnabled = false;
+            }
+            Settings.setAdvancedModeEnabled(advanceModeEnabled);
+        });
+    }
+
+    hideAdvancedSettings() {
+        var el_advanced = document.getElementById('advanced-settings');
+        el_advanced.classList.add('hidden');
+        var button = document.getElementById('simple-advanced-toggle-button');
+        button.textContent = "Switch to Advanced Mode";
+    }
+
+    showAdvancedSettings() {
+        var el_advanced = document.getElementById('advanced-settings');
+        el_advanced.classList.remove('hidden');
+        var button = document.getElementById('simple-advanced-toggle-button');
+        button.textContent = "Switch to Simple Mode";
     }
 
     // Theme handling
@@ -63,9 +103,11 @@ class PageController {
 
     callbackUrlOnKeyDown(event) {
         if (event.key === 'Enter' || event.keyCode === 13) {
-            let url = document.getElementById('callback-url').value;
+            let el = document.getElementById('callback-url');
+            let url = el.value;
             localStorage.setItem('arc-interactive-callback-url', url);
             console.log("set callback-url:", url);
+            el.blur();
         }
     }
 
